e3f6dd699ce835c09a9e0e6c0fd032afef97c8d8
zhangxiangdong <ioszxd@163.com>
2017-09-22 18:34:21 +0800

    创建项目

---
+++ iphoneXDemo/iphoneXDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  iphoneXDemo
+//
+//  Created by 张向东 on 2017/9/22.
+//  Copyright © 2017年 张向东. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
